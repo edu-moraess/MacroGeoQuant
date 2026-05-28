6654bed03459c4e4eeca8f95b35502e84cce37e0
Eduardo Moraes <199427299+edu-moraess@users.noreply.github.com>
2026-05-28 15:06:05 -0300

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,12 @@
 
+# =========================
+# SIMULATION CONFIG
+# =========================
+
+# start of geopolitical shock regime
+GUERRA_START = "2022-02-24"
+
+# Monte Carlo settings
+SIMS = 5000
+STEPS = 30
+MC_SEED = 42
