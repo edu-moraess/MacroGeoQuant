d872ee7efbbfdf2008170a387e267deabae016bb
Eduardo Moraes <199427299+edu-moraess@users.noreply.github.com>
2026-05-28 15:05:29 -0300

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,13 @@
+# =========================
+# MARKET TICKERS
+# =========================
+
+TICKERS = {
+    "oil": "CL=F",
+    "brent": "BZ=F",
+    "gold": "GC=F",
+    "natgas": "NG=F",
+    "wheat": "ZW=F",
+    "corn": "ZC=F",
+    "soy": "ZS=F"
+}
